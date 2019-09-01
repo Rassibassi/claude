2b37a55fc0866d6485c292127c913829520669d8
Rasmus Jones <rasjones@1990gmail.com>
2019-09-01 11:27:34 +0200

    bugfix in parameters of rrcos

--- claude/claude/tx.py
+++ claude/claude/tx.py
@@ -14,7 +14,29 @@ def generateSymbols(constellation,size):
 	constellation = constellation[0,np.random.randint(M, size=size)]
 	return constellation
 
-def rrcos(beta, span, sps):
+def rrcos(beta, sps, span):
+    delay = span*sps/2
+    t     = np.concatenate([np.arange(-delay, delay), np.array([delay])])/sps
+    # Initialization
+    h = np.zeros(t.shape)
+    # find t=0
+    idx1 = np.where(t == 0)
+    if len(idx1):
+        h[idx1] = -1 / (np.pi*sps) * (np.pi*(beta-1) - 4*beta )
+    # find where denominator is zero
+    idx2 = np.where(abs(abs(4*beta*t) - 1.0) < np.sqrt(np.spacing(1)))
+    if len(idx2):
+        h[idx2] = 1 / (2*np.pi*sps) * (  np.pi * (beta + 1) * np.sin( np.pi * (beta + 1) / (4 * beta) ) -4 * beta * np.sin( np.pi * (beta - 1) / (4 * beta) ) + np.pi * (beta - 1) * np.cos( np.pi * (beta - 1) / (4 * beta) ) )
+    # main equation
+    ind    = np.arange(0,len(t))
+    ind    = np.delete(ind,np.concatenate([[idx1], [idx2]], axis=None))
+    nind   = t[ind]
+    h[ind] = -4*beta/sps * ( np.cos( (1+beta)*np.pi*nind) + np.sin( (1-beta)*np.pi*nind) / (4*beta*nind) ) / ( np.pi * ( (4*beta*nind)**2 - 1) )
+    # Normalize filter energy
+    h = h / np.sqrt(np.sum(h**2));
+    return h
+
+def rrcos2(beta, span, sps):
     delay = span * sps / 2
     t     = np.arange(-delay, delay+1) / sps
     pi    = np.pi
