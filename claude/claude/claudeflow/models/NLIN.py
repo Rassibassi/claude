b1cbd530e12e03bb354e91193a19b6885c74f116
Rasmus Jones <rasjones@1990gmail.com>
2019-09-23 07:48:00 +0200

    rename PolMux for nPol inside of tf NLIN model

--- claude/claude/claudeflow/models/NLIN.py
+++ claude/claude/claudeflow/models/NLIN.py
@@ -20,47 +20,47 @@ def tfConstants(dtype=tf.float64):
     c.eightyone = tf.constant(81,dtype)
     return c
 
-def calcInterChannelNLIN(chi,kur,P0,PolMux,dtype=tf.float64):
+def calcInterChannelNLIN(chi,kur,P0,nPol,dtype=tf.float64):
     c = tfConstants(dtype=dtype)
     
     NLIN_inter = chi[0,:]+(kur-c.two)*chi[1,:]
-    if PolMux == 1:
+    if nPol == 2:
         NLIN_inter = c.sixteen/c.eightyone*(NLIN_inter+c.two*chi[0,:]/c.four+(kur-c.two)*chi[1,:]/c.four)
     NLIN_inter = P0**c.three*NLIN_inter
     return NLIN_inter
 
-def calcInterChannelNLINAddTerms(X,kur,P0,PolMux,dtype=tf.float64):
+def calcInterChannelNLINAddTerms(X,kur,P0,nPol,dtype=tf.float64):
     c = tfConstants(dtype=dtype)
 
     NLIN_inter_addTerms = c.four*X[0,:]+c.four*(kur-c.two)*X[1,:]+c.two*X[2,:]+(kur-c.two)*X[3,:]
-    if PolMux == 1:
+    if nPol == 2:
         NLIN_inter_addTerms = c.sixteen/c.eightyone*( NLIN_inter_addTerms+c.two*X[0,:]+(kur-c.two)*X[1,:]+X[2,:] ) # +0*(param.kur-2)*X24)
     NLIN_inter_addTerms = P0**c.three*NLIN_inter_addTerms
     return NLIN_inter_addTerms
 
-def calcIntraChannelNLIN(X,kur,kur3,P0,PolMux,dtype=tf.float64):
+def calcIntraChannelNLIN(X,kur,kur3,P0,nPol,dtype=tf.float64):
     c = tfConstants(dtype=dtype)
 
     NLIN_intra = c.two*X[1,:]+(kur-c.two)*(c.four*X[2,:]+X[3,:])+(kur3-c.nine*kur+c.twelve)*X[4,:]-(kur-c.two)**c.two*X[0,:]
-    if PolMux == 1:
+    if nPol == 2:
         NLIN_intra = c.sixteen/c.eightyone*(NLIN_intra+X[1,:]+(kur-c.two)*X[2,:])
     NLIN_intra = P0**c.three*NLIN_intra
     return NLIN_intra
 
-def calcInterChannelGN(chi,P0,PolMux,dtype=tf.float64):
+def calcInterChannelGN(chi,P0,nPol,dtype=tf.float64):
     c = tfConstants(dtype=dtype)
 
     GN_inter = chi[0,:]
-    if PolMux == 1:
+    if nPol == 2:
         GN_inter = c.sixteen/c.eightyone*(GN_inter+c.two*chi[0,:]/c.four)
     GN_inter = P0**c.three*GN_inter
     return GN_inter
 
-def calcIntraChannelGN(X,P0,PolMux,dtype=tf.float64):
+def calcIntraChannelGN(X,P0,nPol,dtype=tf.float64):
     c = tfConstants(dtype=dtype)
 
     GN_intra = c.two*X[1,:]
-    if PolMux == 1:
+    if nPol == 2:
         GN_intra = c.sixteen/c.eightyone*(GN_intra+X[1,:])
     GN_intra = P0**c.three*GN_intra
     return GN_intra
